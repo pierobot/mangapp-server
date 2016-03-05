78e6d19be8ae6c73f66f5f08db45c68d7f67e398
pierobot <pierobot@users.noreply.github.com>
2016-03-05 16:48:03 -0600

    Added mustache context functions for templates Updated typedefs

--- src/library.hpp
+++ src/library.hpp
@@ -1,8 +1,10 @@
 #ifndef MANGAPP_LIBRARY_HPP
 #define MANGAPP_LIBRARY_HPP
 
+#include "archive.hpp"
 #include "directory_entry.hpp"
 #include "file_enumeration.hpp"
+#include "sort.hpp"
 
 #include <cstdint>
 #include <fstream>
@@ -15,6 +17,8 @@
 #include <utf8/utf8.h>
 
 #include <json11/json11.hpp>
+
+#include <mstch/mstch.hpp>
 
 namespace base
 {
@@ -54,9 +58,10 @@ namespace base
     {
     public:
         typedef size_t key_type;
-        typedef DirectoryEntryType entry_type;
+        typedef DirectoryEntryType directory_entry_type;
+        typedef typename DirectoryEntryType::file_entry_type file_entry_type;
 
-        typedef std::unordered_map<key_type, entry_type> library_map_t;
+        typedef std::unordered_map<key_type, directory_entry_type> library_map_t;
         typedef typename library_map_t::const_iterator const_iterator;
 
         /**
@@ -79,7 +84,7 @@ namespace base
                     std::wstring path_str(path, 0, path_end_pos + 1);
 
                     auto key = boost::hash<std::wstring>()(name_str);
-                    m_entries.emplace(key, entry_type(path_str, name_str, key));
+                    m_entries.emplace(key, directory_entry_type(path_str, name_str, key));
                 });
             }
         }
@@ -109,15 +114,146 @@ namespace base
             return m_entries.cend();
         }
 
-        const_iterator find(size_t key) const
+        const_iterator find(key_type key) const
         {
             return m_entries.find(key);
         }
 
-        size_t size() const
+        key_type size() const
         {
             return m_entries.size();
         }
+
+        mstch::map const get_list_context() const
+        {
+            mstch::array library_array;
+
+            std::vector<std::reference_wrapper<directory_entry_type const>> ordered;
+            for (auto const & pair : m_entries)
+            {
+                ordered.push_back(std::cref(pair.second));
+            }
+
+            std::sort(ordered.begin(), ordered.end(),
+                [](std::reference_wrapper<directory_entry_type const> const & left,
+                   std::reference_wrapper<directory_entry_type const> const & right) -> bool
+            {
+                return std::locale()(left.get().get_name(), right.get().get_name());
+            });
+
+            for (auto const & entry : ordered)
+            {
+                std::wstring utf16_name(entry.get().get_name());
+                std::string utf8_name;
+                utf8::utf16to8(utf16_name.cbegin(), utf16_name.cend(), std::back_inserter(utf8_name));
+
+                library_array.emplace_back(mstch::map{
+                    { "key",  std::to_string(entry.get().get_key()) },
+                    { "name", utf8_name }
+                });
+            }
+
+            return mstch::map({
+                { "library-list", library_array }
+            });
+        }
+
+        mstch::map get_files_context(key_type key) const
+        {
+            auto manga_iterator = library::find(key);
+            if (manga_iterator != library::cend())
+            {
+                std::vector<std::reference_wrapper<library::file_entry_type const>> ordered;
+
+                std::for_each(manga_iterator->second.cbegin(), manga_iterator->second.cend(),
+                    [&ordered](library::directory_entry_type::map_type::value_type const & entry)
+                {
+                    ordered.push_back(std::cref(entry.second));
+                });
+
+                std::sort(ordered.begin(), ordered.end(),
+                    [](std::reference_wrapper<library::file_entry_type const> const & left,
+                        std::reference_wrapper<library::file_entry_type const> const & right)
+                {
+                    return sort::numerical_wregex_less()(left.get().get_name(), right.get().get_name());
+                });
+
+                mstch::array files_array{};
+
+                std::for_each(ordered.cbegin(), ordered.cend(),
+                    [&files_array, key](std::reference_wrapper<library::file_entry_type const> const & value) -> void
+                {
+                    auto const & entry = value.get();
+
+                    if (is_in_container(g_archive_extensions, entry.get_extension()) == true)
+                    {
+                        std::wstring const & utf16_name(entry.get_name());
+                        std::string utf8_name;
+
+                        utf8::utf16to8(utf16_name.cbegin(), utf16_name.cend(), std::back_inserter(utf8_name));
+
+                        files_array.emplace_back(mstch::map({
+                            { "key", std::to_string(key) },
+                            { "file-key", std::to_string(entry.get_key()) },
+                            { "file-name", utf8_name }
+                        }));
+                    }
+                });
+
+                std::wstring const & utf16_name(manga_iterator->second.get_name());
+                std::string utf8_name;
+                utf8::utf16to8(utf16_name.cbegin(), utf16_name.cend(), std::back_inserter(utf8_name));
+
+                return mstch::map({
+                    { "key", std::to_string(key) },
+                    { "name", std::move(utf8_name) },
+                    { "file-count", std::to_string(files_array.size()) },
+                    { "files-list", std::move(files_array) },
+                });
+            }
+
+            return mstch::map({});
+        }
+
+        mstch::map const get_reader_context(key_type key, key_type file_key) const
+        {
+            auto entry_iterator = find(key);
+            if (entry_iterator != cend())
+            {
+                auto archive_iterator = entry_iterator->second.find(file_key);
+                if (archive_iterator != entry_iterator->second.cend())
+                {
+                    auto file_path = archive_iterator->second.get_path() + archive_iterator->second.get_name();
+
+                    auto archive_ptr = mangapp::archive::open(file_path);
+                    if (archive_ptr != nullptr)
+                    {
+                        mstch::array image_array;
+
+                        for (auto const & image_entry : *archive_ptr)
+                        {
+                            image_array.emplace_back(mstch::map({
+                                { "key", std::to_string(key) },
+                                { "file-key", std::to_string(file_key) },
+                                { "index", std::to_string(image_entry->index()) }
+                            }));
+                        }
+
+                        std::wstring const & utf16_name(entry_iterator->second.get_name());
+                        std::string utf8_name;
+                        utf8::utf16to8(utf16_name.cbegin(), utf16_name.cend(), std::back_inserter(utf8_name));
+
+                        return mstch::map({
+                            { "name", utf8_name },
+                            { "image-list", image_array }
+                        });
+                    }
+                }
+            }
+
+            return mstch::map({});
+        }
+
     protected:
     private:
         library_map_t m_entries;
