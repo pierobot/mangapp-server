3dee2f65527b95138c078b4fd912c12dc3d84dfe
pierobot <pierobot@users.noreply.github.com>
2016-03-05 19:11:10 -0600

    Added check for presence of extension in archive::open

--- src/archive.cpp
+++ src/archive.cpp
@@ -9,21 +9,24 @@ namespace mangapp
     std::unique_ptr<archive> archive::open(std::wstring const & file_path)
     {
         auto extension_start_pos = file_path.rfind(L'.');
-        std::wstring extension(file_path.substr(extension_start_pos));
-
-        if (extension == L".zip" || extension == L".cbz")
+        if (extension_start_pos != std::string::npos)
         {
-            return std::make_unique<archive_zip>(file_path);
+            std::wstring extension(file_path.substr(extension_start_pos));
+            
+            if (extension == L".zip" || extension == L".cbz")
+            {
+                return std::make_unique<archive_zip>(file_path);
+            }
+            else if (extension == L".rar" || extension == L".cbr")
+            {
+                return std::make_unique<archive_rar>(file_path);
+            }
+            else if (extension == L".7z" || extension == L".cb7")
+            {
+                return std::make_unique<archive_7z>(file_path);
+            }
         }
-        else if (extension == L".rar" || extension == L".cbr")
-        {
-            return std::make_unique<archive_rar>(file_path);
-        }
-        else if (extension == L".7z" || extension == L".cb7")
-        {
-            return std::make_unique<archive_7z>(file_path);
-        }
-
+       
         return nullptr;
     }
 }
