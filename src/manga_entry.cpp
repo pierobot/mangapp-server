3b2d6c2ac85447c4281d5ebaca4c2baea18cb1c4
pierobot <Xv24xt@airmail.cc>
2016-03-08 17:46:35 -0600

    Removed unused code

--- src/manga_entry.cpp
+++ src/manga_entry.cpp
@@ -6,18 +6,9 @@ namespace mangapp
     manga_entry::manga_entry(std::wstring const & path, std::wstring const & name, size_t key) :
         base::file_entry(path, name, key)
     {
-        std::string utf8_name = to_utf8(name);
-
-        m_json = json11::Json::object{ { "key", std::to_string(key) },
-        { "name", utf8_name } };
     }
 
     manga_entry::~manga_entry()
     {
-    }
-
-    json11::Json const & manga_entry::to_json() const
-    {
-        return m_json;
     }
 }
