b4c86ac62abfe9d439b32b5afc1aaa886c8f273f
pierobot <Xv24xt@airmail.cc>
2016-03-10 09:43:08 -0600

    Improve thumbnail retrieval code

--- src/directory_entry.hpp
+++ src/directory_entry.hpp
@@ -14,6 +14,19 @@
 #include <boost/range/iterator_range.hpp>
 #include <boost/filesystem.hpp>
 
+static std::vector<std::wstring> const g_image_extensions = { L".jpg", L".jpeg", L".png",
+                                                              L".JPG", L".JPEG", L".PNG" };
+
+static std::vector<std::wstring> const g_archive_extensions = { L".rar", L".cbr", L".RAR", L".CBR",
+                                                                L".zip", L".cbz", L".ZIP", L".CBZ",
+                                                                L".7z",  L".cb7", L".7Z", L".CB7" };
+
+template<class Container, class Element>
+bool is_in_container(Container const & container, Element const & element)
+{
+    return std::find(container.cbegin(), container.cend(), element) != container.cend();
+}
+
 namespace base
 {
     template<class FileEntryType>
@@ -32,15 +45,25 @@ namespace base
             m_key(key)
         {
             enumerate_files(path + name, file_search_flags::FlagFile, false,
-                [this, &path](std::wstring const & full_path, file_search_flags flag)
+                [this, &path](std::wstring const & file_path, file_search_flags flag)
             {
-                auto path_end_pos = full_path.rfind(L"/");
-                
-                std::wstring name_str = full_path.substr(path_end_pos + 1);
-                std::wstring path_str(full_path, 0, path_end_pos + 1);
+                auto extension_start_pos = file_path.rfind(L'.');
+                if (extension_start_pos != std::string::npos)
+                {
+                    auto extension_str = file_path.substr(extension_start_pos);
+                    // We only want archives and images
+                    if (is_in_container(g_archive_extensions, extension_str) == true ||
+                        is_in_container(g_image_extensions, extension_str) == true)
+                    {
+                        auto path_end_pos = file_path.rfind(L"/");
 
-                auto key = boost::hash<std::wstring>()(name_str);
-                m_files.emplace(key, file_entry_type(path_str, name_str, key));
+                        std::wstring name_str = file_path.substr(path_end_pos + 1);
+                        std::wstring path_str(file_path, 0, path_end_pos + 1);
+
+                        auto key = boost::hash<std::wstring>()(name_str);
+                        m_files.emplace(key, file_entry_type(path_str, name_str, key));
+                    }
+                }
             });
         }
 
@@ -62,6 +85,11 @@ namespace base
         const_iterator find(size_t key) const
         {
             return m_files.find(key);
+        }
+
+        size_t const size() const
+        {
+            return m_files.size();
         }
 
         std::wstring const & get_path() const
