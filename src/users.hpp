5b9896b03b77b4be23ebd173c6def5f167bad0a2
pierobot <pierobot@users.noreply.github.com>
2016-04-07 12:46:47 -0500

    Added std::mutex to users Forgot users.cpp and hpp files

---
+++ src/users.hpp
@@ -0,0 +1,42 @@
+#ifndef MANGAPP_USERS_HPP
+#define MANGAPP_USERS_HPP
+
+#include <mutex>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace json11
+{
+    class Json;
+}
+
+namespace mangapp
+{
+    struct user_data
+    {
+        std::string m_name;
+        std::string m_password;
+        std::vector<std::string> m_session_ids;
+        std::vector<size_t> m_accessible_paths;
+    };
+
+    class users
+    {
+    public:
+        users(json11::Json const & users_json, json11::Json const & manga_json);
+        ~users();
+
+        bool authenticate(std::string const & user, std::string const & password, std::string & session_id);
+        bool is_authenticated(std::string const & session_id);
+        void logout(std::string const & session_id);
+
+        bool can_access(std::string const & session_id, size_t path_key);
+    protected:
+    private:
+        std::unordered_map<std::string, user_data> m_users;
+        std::mutex m_users_mutex;
+    };
+}
+
+#endif
